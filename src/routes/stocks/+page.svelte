c11e76066a577534a7d38d9a2c01cb7109f1f309
Josef Kuchař <josef.kuchar267@gmail.com>
2024-02-22 16:04:47 +0100

    Basic fetch

---
+++ src/routes/stocks/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { PageData } from './$types'
+
+  export let data: PageData
+</script>
+
+<div>
+  <h1>My Blog</h1>
+  <main>
+    <div>
+      {JSON.stringify(data.stocks)}
+    </div>
+  </main>
+</div>

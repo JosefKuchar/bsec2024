598ca885ecd04858654a7bf5ea5bfd227290987e
Josef Kuchař <josef.kuchar267@gmail.com>
2024-02-22 16:43:44 +0100

    All table features

--- src/routes/stocks/+page.svelte
+++ src/routes/stocks/+page.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+	import Search from '$lib/components/client/Search.svelte';
+	import ThFilter from '$lib/components/client/ThFilter.svelte';
+	import ThSort from '$lib/components/client/ThSort.svelte';
+	import RowCount from '$lib/components/client/RowCount.svelte';
+	import RowsPerPage from '$lib/components/client/RowsPerPage.svelte';
 	import Pagination from '$lib/components/client/Pagination.svelte';
 	import { DataHandler } from '@vincjo/datatables';
 	import type { PageData } from './$types';
@@ -12,21 +17,33 @@
 	<h1>My Blog</h1>
 	<main>
 		<div class="table-container space-y-4">
+			<header class="flex justify-between gap-4">
+				<Search {handler} />
+				<RowsPerPage {handler} />
+			</header>
 			<table class="table table-hover table-compact table-auto w-full">
+				<thead>
+					<tr>
+						<ThSort {handler} orderBy="name">Název</ThSort>
+					</tr>
+					<tr>
+						<ThFilter {handler} filterBy="name" />
+					</tr>
+				</thead>
 				<tbody>
 					{#each $rows as row}
 						<tr>
 							<td>{row.name}</td>
-							<td>{row.negative}</td>
+							<!-- <td>{row.negative}</td> -->
 							<!-- <td>{row.email}</td> -->
 						</tr>
 					{/each}
 				</tbody>
-				<footer class="flex justify-between">
-					<!-- <RowCount {handler} /> -->
-					<Pagination {handler} />
-				</footer>
 			</table>
+			<footer class="flex justify-between">
+				<!-- <RowCount {handler} /> -->
+				<Pagination {handler} />
+			</footer>
 		</div>
 	</main>
 </div>

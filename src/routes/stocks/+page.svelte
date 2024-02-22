90fab9f1ca367f9bd2aa32312dbe0deef6098a28
Josef Kuchař <josef.kuchar267@gmail.com>
2024-02-22 21:17:43 +0100

    Detail edit

--- src/routes/stocks/+page.svelte
+++ src/routes/stocks/+page.svelte
@@ -8,14 +8,18 @@
 	import { DataHandler } from '@vincjo/datatables';
 	import type { PageData } from './$types';
 	import AmountColor from '$lib/components/AmountColor.svelte';
+	import { goto } from '$app/navigation';
 
 	export let data: PageData;
 	const handler = new DataHandler(data.stocks, { rowsPerPage: 10 });
 	const rows = handler.getRows();
+
+	const handleEditClick = (stockId: number) => {
+		goto(`/stocks/${stockId}`);
+	};
 </script>
 
 <div>
-	<h1>My blog</h1>
 	<main>
 		<div class="table-container space-y-4">
 			<header class="flex justify-between gap-4">
@@ -31,9 +35,12 @@
 						<ThSort {handler} orderBy="min">Min. cena</ThSort>
 						<ThSort {handler} orderBy="max">Max. cena</ThSort>
 						<ThSort {handler} orderBy="rate">Aktuální cena</ThSort>
-						<ThSort {handler} orderBy="negative"><i class="las la-frown text-xl"></i> scénář</ThSort>
+						<ThSort {handler} orderBy="negative"><i class="las la-frown text-xl"></i> scénář</ThSort
+						>
 						<ThSort {handler} orderBy="neutral"><i class="las la-meh text-xl"></i> scénář</ThSort>
-						<ThSort {handler} orderBy="positive"><i class="las la-laugh text-xl"></i> scénář</ThSort>
+						<ThSort {handler} orderBy="positive"><i class="las la-laugh text-xl"></i> scénář</ThSort
+						>
+						<th></th>
 					</tr>
 					<tr>
 						<ThFilter {handler} filterBy="name" />
@@ -45,11 +52,12 @@
 						<ThFilter {handler} filterBy="negative" />
 						<ThFilter {handler} filterBy="neutral" />
 						<ThFilter {handler} filterBy="positive" />
+						<th></th>
 					</tr>
 				</thead>
 				<tbody>
 					{#each $rows as row}
-						<tr>
+						<tr class="cursor-pointer" on:click={() => handleEditClick(row.id)}>
 							<td>{row.name}</td>
 							<td>{row.bic}</td>
 							<td>{row.isin}</td>
@@ -59,6 +67,13 @@
 							<td><AmountColor value={row.negative}>{row.negative} %</AmountColor></td>
 							<td><AmountColor value={row.neutral}>{row.neutral} %</AmountColor></td>
 							<td><AmountColor value={row.positive}>{row.positive} %</AmountColor></td>
+							<td
+								><a href="/todo"
+									><button type="button" class="btn-icon variant-filled-primary"
+										><i class="las la-money-check-alt"></i></button
+									></a
+								></td
+							>
 						</tr>
 					{/each}
 				</tbody>

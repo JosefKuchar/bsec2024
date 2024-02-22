d1547eb23fb7bbb0fd871a44c3ba8359d0a46cc2
Veronika Nevařilová <xnevar00@stud.fit.vutbr.cz>
2024-02-22 16:56:22 +0100

    Customize sidebar

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,28 +1,45 @@
 <script lang="ts">
     import { AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
+    import cash_white from '../assets/cash_white.svg';
+    import home_white from '../assets/home_white.svg';
+    import graph_white from '../assets/graph_white.svg';
+    import invest_white from '../assets/invest_white.svg';
     let currentTile: number = 0;
 
 </script>
 
 <AppRail>
-	<svelte:fragment slot="lead">
-		<AppRailAnchor href="/" >(icon)</AppRailAnchor>
-	</svelte:fragment>
-	<!-- --- -->
 	<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
-		<svelte:fragment slot="lead">(icon)</svelte:fragment>
-		<span>Tile 1</span>
-	</AppRailTile>
-	<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
-		<svelte:fragment slot="lead">(icon)</svelte:fragment>
-		<span>Tile 2</span>
-	</AppRailTile>
-	<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
-		<svelte:fragment slot="lead">(icon)</svelte:fragment>
-		<span>Tile 3</span>
+		<svelte:fragment slot="lead">
+            <div class="flex items-center justify-center">
+                <img src={home_white} alt="Description" class="h-8 w-8 flex items-center justify-center">
+            </div>
+        </svelte:fragment>
+		<span>Domů</span>
 	</AppRailTile>
 	<!-- --- -->
-	<svelte:fragment slot="trail">
-		<AppRailAnchor href="/" target="_blank" title="Account">(icon)</AppRailAnchor>
-	</svelte:fragment>
+	<AppRailTile bind:group={currentTile} name="tile-1" value={1} title="tile-1">
+		<svelte:fragment slot="lead">
+            <div class="flex items-center justify-center">
+                <img src={cash_white} alt="Description" class="h-8 w-8">
+            </div>
+        </svelte:fragment>
+		<span>Příjmy a výdaje</span>
+	</AppRailTile>
+	<AppRailTile bind:group={currentTile} name="tile-2" value={2} title="tile-2">
+		<svelte:fragment slot="lead">
+            <div class="flex items-center justify-center">
+                <img src={invest_white} alt="Description" class="h-8 w-8">
+            </div>
+        </svelte:fragment>
+		<span>Investice</span>
+	</AppRailTile>
+	<AppRailTile bind:group={currentTile} name="tile-3" value={3} title="tile-3">
+		<svelte:fragment slot="lead">
+            <div class="flex items-center justify-center">
+                <img src={graph_white} alt="Description" class="h-8 w-8">
+            </div>
+        </svelte:fragment>
+		<span>Grafy</span>
+	</AppRailTile>
 </AppRail>

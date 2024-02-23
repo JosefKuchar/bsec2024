83641749816b674f5d12ae3da1acc86569664f5b
JachymD <jachym.dolezal@gmail.com>
2024-02-23 02:12:38 +0100

    add formatted edit

--- src/routes/change/[id]/+page.svelte
+++ src/routes/change/[id]/+page.svelte
@@ -38,56 +38,75 @@
 	$: filteredTypes = data.changeTypes.filter((type) => type.dir === data.data.dir);
 </script>
 
-<div>
+<div class="felx col-auto bg-clip-padding">
 	<!-- input suma -->
-	<input class="input" type="search" name="demo" bind:value={inputType} placeholder="Search..." />
-	<div class="card w-full max-w-sm max-h-48 p-4 overflow-y-auto" tabindex="-1">
-		<Autocomplete bind:input={inputType} options={filteredTypes} on:selection={onFlavorSelection} />
+	<div>
+		<span class="text-lg font-bold">Typ</span>
 	</div>
-
-	<input class="input" type="number" placeholder="Suma" bind:value={data.data.amount} />
-
-	<RadioGroup>
-		<RadioItem bind:group={data.data.frequency} name="justify" value={Frequency.OneTime}
-			>Jednorázově</RadioItem
-		>
-		<RadioItem bind:group={data.data.frequency} name="justify" value={Frequency.Daily}
-			>Denně</RadioItem
-		>
-		<RadioItem bind:group={data.data.frequency} name="justify" value={Frequency.Monthly}
-			>Týdně</RadioItem
-		>
-		<RadioItem bind:group={data.data.frequency} name="justify" value={Frequency.Weekly}
-			>Měsíčně</RadioItem
-		>
-		<RadioItem bind:group={data.data.frequency} name="justify" value={Frequency.Yearly}
-			>Ročně</RadioItem
-		>
-	</RadioGroup>
-	<!-- time picker -->
-
-	<!-- add button -->
-	{#if data.data.frequency === Frequency.OneTime}
-		<input
-			class="input w-40"
-			title="Input (date)"
-			type="date"
-			bind:value={data.data.from}
-		/>
-	{:else}
-		<input
-			class="input w-40"
-			title="Input (date)"
-			type="date"
-			bind:value={data.data.from}
-		/>
-
-		<input
-			class="input w-40"
-			title="Input (date)"
-			type="date"
-			bind:value={data.data.to}
-		/>
-	{/if}
-	<button type="button" class="btn variant-filled" on:click={handleSubmit}>Uložit</button>
+	<div class="py-8">
+		<input class="input" type="search" name="demo" bind:value={inputType} placeholder="Search..." />
+		<div class="card w-full max-w-sm max-h-48 p-4 overflow-y-auto" tabindex="-1">
+			<Autocomplete bind:input={inputType} options={filteredTypes} on:selection={onFlavorSelection} />
+		</div>
+	</div>
+	<div>
+		<span class="text-lg font-bold">Suma</span>
+	</div>
+	<div class="py-4">
+		<input class="input" type="number" placeholder="Suma" bind:value={data.data.amount} />
+	</div>
+	<div>
+		<span class="text-lg font-bold">Frekvence</span>
+	</div>
+	<div class="py-4">
+		<RadioGroup>
+			<RadioItem bind:group={data.data.frequency} name="justify" value={Frequency.OneTime}
+				>Jednorázově</RadioItem
+			>
+			<RadioItem bind:group={data.data.frequency} name="justify" value={Frequency.Daily}
+				>Denně</RadioItem
+			>
+			<RadioItem bind:group={data.data.frequency} name="justify" value={Frequency.Monthly}
+				>Týdně</RadioItem
+			>
+			<RadioItem bind:group={data.data.frequency} name="justify" value={Frequency.Weekly}
+				>Měsíčně</RadioItem
+			>
+			<RadioItem bind:group={data.data.frequency} name="justify" value={Frequency.Yearly}
+				>Ročně</RadioItem
+			>
+		</RadioGroup>
+	</div>
+	<div>
+		<span class="text-lg font-bold">Od</span>
+	</div>
+	<div class="py-4">
+		{#if data.data.frequency === Frequency.OneTime}
+			<input
+				class="input w-40"
+				title="Input (date)"
+				type="date"
+				bind:value={data.data.from}
+			/>
+		{:else}
+			<input
+				class="input w-40"
+				title="Input (date)"
+				type="date"
+				bind:value={data.data.from}
+			/>
+			<div>
+				<span class="text-lg font-bold">Do</span>
+			</div>
+			<input
+				class="input w-40"
+				title="Input (date)"
+				type="date"
+				bind:value={data.data.to}
+			/>
+		{/if}
+	</div>
+	<div class="py-4">
+		<button type="button" class="btn variant-filled" on:click={handleSubmit}>Uložit</button>
+	</div>
 </div>

b001ac95a13b96926d03f5b3de8439fcc7018e13
Lukas Vecerka <vecerkalukas18@gmail.com>
2024-02-23 03:10:10 +0100

    creation of new investments

--- src/lib/components/AmountColor.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    export let value: number;
-</script>
-
-<span class:text-success-500={value > 0} class:text-error-500={value < 0}>
-    <slot />
-</span>
--- src/lib/store.ts
+++
@@ -1,4 +0,0 @@
-import { writable } from 'svelte/store';
-import { getTodayFormatted } from '$lib/utils.ts';
-
-export const selectedDate = writable(getTodayFormatted());

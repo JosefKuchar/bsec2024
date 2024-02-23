1159fe629111ff2301c6bb0d9d68fcb6137c9734
Lukas Vecerka <vecerkalukas18@gmail.com>
2024-02-23 06:28:01 +0100

    changed avatar

--- src/lib/components/PageHeader.svelte
+++ src/lib/components/PageHeader.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { AppBar, Avatar } from '@skeletonlabs/skeleton';
     import { getTodayFormatted } from '$lib/utils';
+    import AvatarIcon from '$lib/assets/avatar.jpg';
 
 </script>
 
@@ -8,6 +9,6 @@
 <AppBar background="bg-transparent" slotTrail="place-content-end">
     <svelte:fragment slot="trail">
         <h3 class="h3">Jan Novák</h3>
-        <Avatar initials="JN" background="bg-surface-700" />
+        <Avatar src={AvatarIcon} background="bg-surface-700" />
     </svelte:fragment>
 </AppBar>

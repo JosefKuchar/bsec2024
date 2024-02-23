ba9ef08b883bc10bf3da8645c58e5c5d3ca80dca
JachymD <jachym.dolezal@gmail.com>
2024-02-23 05:40:54 +0100

    add fix filtering and date view

--- src/routes/investments/+page.server.ts
+++ src/routes/investments/+page.server.ts
@@ -1,6 +1,7 @@
 import prisma from '$lib/prisma';
 import type { PageServerLoad } from './$types';
 import { getFrequencyString } from '$lib/utils';
+import moment from 'moment';
 
 export const load = (async () => {
 	const investmentChange = await prisma.investmentChange.findMany({
@@ -11,6 +12,10 @@ export const load = (async () => {
 
 	return { investmentChange: investmentChange.map((investment) => ({
 		...investment,
-		frequency: getFrequencyString(investment.frequency)
+		frequency: getFrequencyString(investment.frequency),
+		name: investment.investment.name,
+		bic: investment.investment.bic,
+		to : moment(investment.to).format('DD.MM.YYYY'),
+		from : moment(investment.from).format('DD.MM.YYYY')
 	}))};
 }) satisfies PageServerLoad;

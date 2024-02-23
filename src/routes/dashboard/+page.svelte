ae38cd5d43fc2ba1ff0ca34b7301e4aaf41464e7
Josef Kuchař <josef.kuchar267@gmail.com>
2024-02-23 03:10:54 +0100

    fix

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -5,105 +5,100 @@
 	import { Dir, Frequency } from '$lib/enums';
 	import moment from 'moment';
 	import { frequencyToDays } from '$lib/utils';
-    import { ComboChart } from '@carbon/charts-svelte'
+	import { ComboChart } from '@carbon/charts-svelte';
 
 	export let data: PageData;
 	let graphValues = [];
 
-    let data2 = [
-  {
-    "group": "Volné prostředky",
-    "date": "2018-12-31T23:00:00.000Z",
-    "value": 10000
-  },
-  {
-    "group": "Volné prostředky",
-    "date": "2019-01-04T23:00:00.000Z",
-    "value": 65000
-  },
-  {
-    "group": "Volné prostředky",
-    "date": "2019-01-07T23:00:00.000Z",
-    "value": 10000
-  },
-  {
-    "group": "Volné prostředky",
-    "date": "2019-01-12T23:00:00.000Z",
-    "value": 49213
-  },
-  {
-    "group": "Investice",
-    "date": "2019-01-16T23:00:00.000Z",
-    "value": 51213
-  },
-  {
-    "group": "Investice",
-    "date": "2018-12-31T23:00:00.000Z",
-    "value": 20000
-  },
-  {
-    "group": "Investice",
-    "date": "2019-01-04T23:00:00.000Z",
-    "value": 25000
-  },
-  {
-    "group": "Investice",
-    "date": "2019-01-07T23:00:00.000Z",
-    "value": 60000
-  },
-  {
-    "group": "Investice",
-    "date": "2019-01-12T23:00:00.000Z",
-    "value": 30213
-  },
-  {
-    "group": "Investice",
-    "date": "2019-01-16T23:00:00.000Z",
-    "value": 55213
-  },
-]
+	let data2 = [
+		{
+			group: 'Volné prostředky',
+			date: '2018-12-31T23:00:00.000Z',
+			value: 10000
+		},
+		{
+			group: 'Volné prostředky',
+			date: '2019-01-04T23:00:00.000Z',
+			value: 65000
+		},
+		{
+			group: 'Volné prostředky',
+			date: '2019-01-07T23:00:00.000Z',
+			value: 10000
+		},
+		{
+			group: 'Volné prostředky',
+			date: '2019-01-12T23:00:00.000Z',
+			value: 49213
+		},
+		{
+			group: 'Investice',
+			date: '2019-01-16T23:00:00.000Z',
+			value: 51213
+		},
+		{
+			group: 'Investice',
+			date: '2018-12-31T23:00:00.000Z',
+			value: 20000
+		},
+		{
+			group: 'Investice',
+			date: '2019-01-04T23:00:00.000Z',
+			value: 25000
+		},
+		{
+			group: 'Investice',
+			date: '2019-01-07T23:00:00.000Z',
+			value: 60000
+		},
+		{
+			group: 'Investice',
+			date: '2019-01-12T23:00:00.000Z',
+			value: 30213
+		},
+		{
+			group: 'Investice',
+			date: '2019-01-16T23:00:00.000Z',
+			value: 55213
+		}
+	];
 
-let options = {
-  "title": "Vývoj portfolia",
-  "axes": {
-    "left": {
-      "title": "Hodnota (Kč)",
-      "stacked": true,
-      "mapsTo": "value",
-      "titleOrientation": "right"
-    },
-    "bottom": {
-      "scaleType": "time",
-      "mapsTo": "date"
-    },
-
-  },
-  "curve": "curveMonotoneX",
-  "comboChartTypes": [
-    {
-      "type": "stacked-area",
-      "options": {
-        "points": {
-          "enabled": false
-        }
-      },
-      "correspondingDatasets": [
-        "Investice",
-        "Volné prostředky",
-      ]
-    },
-  ],
-  "color" : {
-    "scale" :
-        {
-        "Volné prostředky" : "#3B82F6",
-        "Investice" : "#EAB308"
-    },
-    "" : "#ffffff"
-  },
-  "height": "400px",
-  "theme": "g100"
-}
+	let options = {
+		title: 'Vývoj portfolia',
+		axes: {
+			left: {
+				title: 'Hodnota (Kč)',
+				stacked: true,
+				mapsTo: 'value',
+				titleOrientation: 'right'
+			},
+			bottom: {
+				scaleType: 'time',
+				mapsTo: 'date'
+			}
+		},
+		curve: 'curveMonotoneX',
+		comboChartTypes: [
+			{
+				type: 'stacked-area',
+				options: {
+					points: {
+						enabled: false
+					}
+				},
+				correspondingDatasets: ['Investice', 'Volné prostředky']
+			}
+		],
+		color: {
+			scale: {
+				'Volné prostředky': '#3B82F6',
+				Investice: '#EAB308'
+			},
+			'': '#ffffff'
+		},
+		height: '400px',
+		theme: 'g100'
+	};
 
 	onMount(() => {
 		let stocks: { [key: number]: number } = {};
@@ -180,9 +175,8 @@ let options = {
 	});
 </script>
 
-
 <h1 class="h1">Graf</h1>
 <div class="w-3/4">
-    <ComboChart style="background:transparent" {data2} {options} />
+	<ComboChart style="background:transparent" data={data2} {options} />
 </div>
 <div class="p-10"></div>
